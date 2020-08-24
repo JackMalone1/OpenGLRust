f0ef3bdce83e2bf9d4e71b9bad20a87d23253eab
Jack Malone <c00236428@itcarlow.ie>
2020-08-24 22:58:17 +0100

    changed some files

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 extern crate sdl2;
 extern crate gl;
 
+pub mod render_gl;
+
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
@@ -50,43 +52,4 @@ fn main() {
     }
 }
 
-fn shader_from_source(source: &CStr, kind: gl::types::GLenum) -> Result<gl::types::GLuint, String>{
-    let id = unsafe{gl::CreateShader(kind)};
 
-    unsafe{
-        gl::ShaderSource(id, 1, &source.as_ptr(), std::ptr::null());
-        gl::CompileShader(id);
-    }
-
-    let mut success: gl::types::GLint = 1;
-
-    unsafe{
-        gl::GetShaderiv(id, gl::COMPILE_STATUS, &mut success);
-    }
-
-    if success == 0{
-        let mut len: gl::types::GLint = 0;
-        unsafe {
-            gl::GetShaderiv(id, gl::INFO_LOG_LENGTH, &mut len);
-        }
-
-        let mut buffer: Vec<u8> = Vec::with_capacity(len as usize + 1);
-
-        buffer.extend([b' '].iter().cycle().take(len as usize));
-
-        let error : CString = unsafe {CString::from_vec_unchecked(buffer)};
-
-        unsafe {
-            gl::GetShaderInfoLog(
-                id,
-                len,
-                std::ptr::null_mut(),
-                error.as_ptr() as *mut gl::types::GLchar
-            );
-
-            return Err(error.to_string_lossy().into_owned());
-        }
-    }
-
-    return Ok(id);
-}
